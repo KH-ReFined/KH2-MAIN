dd83f23f0f7f3cef58f10ac5c84cd3bde5ecbc47
Topaz Whitelock <topaztk@yahoo.com>
2025-04-21 16:12:52 +0200

    LET'S GO, BABEY!

---
+++ mapscripts/es00.asm
@@ -0,0 +1,48 @@
+Program 0x3C
+Bgm Default Default
+Layout "menu/%s/jiminy.bar"
+MapScript 4
+AreaSettings 0 -1
+	SetProgressFlag 0x409
+	SetEvent "999" Type 66
+	SetPartyMenu 0
+
+AreaSettings 7 -1
+	SetJump Type 2 World ES Area 0 Entrance 0 LocalSet 61 FadeType 16385
+	SetPartyMenu 0
+
+AreaSettings 8 -1
+	SetJump Type 5 World ES Area 0 Entrance 0 LocalSet 0 FadeType 16385
+	SetPartyMenu 0
+
+Program 0x3A
+Bgm Default Default
+Layout "menu/%s/jiminy.bar"
+MapScript 3
+AreaSettings 0 -1
+	SetEvent "998" Type 66
+	SetPartyMenu 0
+
+AreaSettings 7 -1
+	SetJump Type 2 World ES Area 0 Entrance 0 LocalSet 60 FadeType 16385
+	SetPartyMenu 0
+
+AreaSettings 8 -1
+	SetJump Type 5 World ES Area 0 Entrance 0 LocalSet 0 FadeType 16385
+	SetPartyMenu 0
+
+Program 0x3D
+Bgm Default Default
+Layout "menu/%s/jiminy.bar"
+AreaSettings 0 -1
+	SetProgressFlag 0x40A
+	SetEvent "993" Type 66
+	SetJump Type 5 World ES Area 0 Entrance 0 LocalSet 0 FadeType 16385
+	SetPartyMenu 0
+
+Program 0x33
+Bgm Default Default
+AreaSettings 0 -1
+	SetEvent "995" Type 67
+	SetJump Type 2 World ES Area 0 Entrance 0 LocalSet 58 FadeType 1
+	SetPartyMenu 0
